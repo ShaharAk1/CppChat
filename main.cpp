53a7eb4104f357354eda08eeddb9a3727e4e60d5
shaharAk <shahar.akiva060603@gmail.com>
2025-11-25 00:50:20 +0200

    added basic style to each window

--- main.cpp
+++ main.cpp
@@ -2,6 +2,9 @@
 #include <QMainWindow>
 #include <QTextEdit>
 #include <QTimer>
+#include <QWidget>
+#include <QVBoxLayout>
+#include "main.h"
 
 int main(int argc, char *argv[])
 {
@@ -9,18 +12,19 @@ int main(int argc, char *argv[])
 
     // ------------------- Server Window -------------------
     QMainWindow serverWindow;
-    serverWindow.setWindowTitle("Server Window");
+    serverWindow.setWindowTitle("Server Window: Person1");
 
+    // QTextEdit
     QTextEdit* serverText = new QTextEdit();
-    serverText->setText("Server: Waiting for messages...");
-    serverText->setStyleSheet("background-color: lightyellow; color: blue; font-size:14pt;");
-    serverWindow.setCentralWidget(serverText);
+    serverText->setText("Person1: Waiting for messages...");
+    serverText->setStyleSheet("background-color: #A8C9FF; color: blue; font-size:14pt; padding: 20px;");
 
+    createChatStyle(serverWindow, serverText);
     serverWindow.show();
 
     // placement in screen
     QTimer::singleShot(0, [&serverWindow](){
-        serverWindow.setGeometry(100, 100, 800, 400);
+        serverWindow.setGeometry(300, 100, 400, 500);
     });
 
     // messages
@@ -32,18 +36,19 @@ int main(int argc, char *argv[])
 
     // ------------------- Client Window -------------------
     QMainWindow clientWindow;
-    clientWindow.setWindowTitle("Client Window");
+    clientWindow.setWindowTitle("Client Window: Person 2");
 
     QTextEdit* clientText = new QTextEdit();
-    clientText->setText("Client: Ready to send messages...");
-    clientText->setStyleSheet("background-color: lightblue; color: darkred; font-size:14pt;");
+    clientText->setText("Person2: Ready to send messages...");
+    clientText->setStyleSheet("background-color: #A8C9FF; color: darkred; font-size:14pt; padding: 20px;");
     clientWindow.setCentralWidget(clientText);
 
+    createChatStyle(clientWindow, clientText);
     clientWindow.show();
 
     //placement in screen
     QTimer::singleShot(0, [&clientWindow](){
-        clientWindow.setGeometry(950, 100, 800, 400);
+        clientWindow.setGeometry(800, 100, 400, 500);
     });
 
     // messages
@@ -54,4 +59,21 @@ int main(int argc, char *argv[])
     clientTimer.start(1000);
 
     return app.exec();
+}
+
+void createChatStyle(QMainWindow& window , QTextEdit* text) {
+
+    // central widget ו-layout
+    QWidget *central = new QWidget(&window);
+    QVBoxLayout *layout = new QVBoxLayout(central);
+
+    // top panel
+    QWidget *square = new QWidget();
+    square->setFixedSize(400, 50);
+    square->setStyleSheet("background-color: #798ABA;");
+
+    layout->addWidget(square);     // הריבוע למעלה
+    layout->addWidget(text);
+
+    window.setCentralWidget(central);
 }
---
+++ main.h
@@ -0,0 +1,10 @@
+//
+// Created by Shahar Akiva on 25/11/2025.
+//
+
+#ifndef MAIN_H
+#define MAIN_H
+
+void createChatStyle(QMainWindow& window , QTextEdit* text);
+
+#endif //MAIN_H
